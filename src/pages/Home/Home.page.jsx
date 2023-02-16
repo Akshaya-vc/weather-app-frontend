b91065388148065e951b32cfb30ebadef4b1c9a6
Akshaya-vc <akshayav7c@gmail.com>
2023-02-16 13:39:56 +0530

    add: search bar

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Home.page.css";
 
 const Home = () => {
-  return <div className="home">This is home page.</div>;
+  const searchIconLink =
+    "https://assets.stickpng.com/images/585e4ae1cb11b227491c3393.png";
+  const [input, setInput] = useState("");
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    // fetch API
+  };
+
+  return (
+    <div className="home">
+      <div className="search">
+        <input
+          type="text"
+          className="search-input"
+          onChange={handleChange}
+          value={input}
+        />
+        <button type="submit" className="search-btn" onClick={handleSubmit}>
+          <img
+            className="search-btn-icon"
+            src={searchIconLink}
+            alt="search-icon"
+          />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
