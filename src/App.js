7e0cd85830aa2c5e4015a5895a694a3d379ed593
Akshaya-vc <akshayav7c@gmail.com>
2023-02-16 12:50:51 +0530

    add: basic file sturcture

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Home from "./pages/Home/Home.page";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
