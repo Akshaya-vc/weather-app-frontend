129f4f7c477227091bc66eeb3553a2ff318926d5
Akshaya-vc <akshayav7c@gmail.com>
2023-02-17 10:51:36 +0530

    add: loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <img
+      src="https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/200w.gif?cid=82a1493bmf0tu9mjb1hfexvl9ojm3s8933t6ia6vbvicb94o&rid=200w.gif&ct=g"
+      alt="loader"
+    />
+  );
+};
+
+export default Loader;
--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -7,6 +7,7 @@ import Scale from "../../components/Scale/Scale.component";
 import ParameterCard from "../../components/ParameterCard/ParameterCard.component";
 
 import parameters from "../../assets/parameters";
+import Loader from "../../components/Loader/Loader";
 
 const SearchIcon =
   "https://www.freeiconspng.com/thumbs/search-icon-png/search-icon-png-17.png";
@@ -14,6 +15,7 @@ const SearchIcon =
 const Home = () => {
   const [input, setInput] = useState("");
   const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   const [toggleScale, setToggleScale] = useState({
     temperature: false,
@@ -23,12 +25,16 @@ const Home = () => {
     setInput(e.target.value);
   };
 
-  const handleSubmit = (location) => {
+  const getForecast = (location) => {
     // fetch API
+    setIsLoading(true);
     fetchForecast(location)
       .then((res) => {
         console.log(res.data);
         setData(res.data);
+      })
+      .then(() => {
+        setIsLoading(false);
       })
       .catch((err) => {
         alert("Something went wrong");
@@ -36,93 +42,103 @@ const Home = () => {
       });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getForecast(input);
+  };
+
   // load initial data on refresh
   useEffect(() => {
-    handleSubmit("London");
+    getForecast("London");
   }, []);
 
   // to switch between two temperature scales
   const handleTempToggle = () => {
     setToggleScale({ ...toggleScale, temperature: !toggleScale.temperature });
   };
-
-  return (
-    <div className="home">
-      {/* Navbar */}
-      <div className="navbar">
-        {/* Search bar */}
-        <div className="search">
-          <input
-            type="text"
-            className="search-input"
-            onChange={handleChange}
-            value={input}
-            placeholder="Search for cities..."
-          />
-          <button
-            type="submit"
-            className="search-btn"
-            onClick={() => handleSubmit(input)}
-          >
-            <img
-              className="search-btn-icon"
-              src={SearchIcon}
-              alt="search-icon"
+  // return <Loader />;
+  if (isLoading) return <Loader />;
+  else
+    return (
+      <div className="home">
+        {/* Navbar */}
+        <div className="navbar">
+          {/* Search bar */}
+          <form className="search" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="search-input"
+              onChange={handleChange}
+              value={input}
+              placeholder="Search for cities..."
             />
-          </button>
-        </div>
-        <Scale handleTempToggle={handleTempToggle} toggleScale={toggleScale} />
-      </div>
-
-      {Object.keys(data).length !== 0 ? (
-        <div className="body">
-          {/* Location header */}
-          <div className="body-item location">
-            <h2>{data.location.name}</h2>
-            <h3>
-              {data.location.region} {data.location.region ? ", " : " "}
-              {data.location.country}
-            </h3>
-          </div>
-
-          <div className="body-item current">
-            {/* Column 1 */}
-            <div className="overview">
+            <button type="submit" className="search-btn" onClick={handleSubmit}>
               <img
-                src={data.current.condition.icon}
-                className="main-icon"
-                alt="weather-icon"
+                className="search-btn-icon"
+                src={SearchIcon}
+                alt="search-icon"
               />
-              <div className="overview-text">
-                {toggleScale.temperature ? (
-                  <h1>{data.current.temp_f}&#8457;</h1>
-                ) : (
-                  <h1>{data.current.temp_c}&#8451;</h1>
-                )}
-                <h3>{data.current.condition.text}</h3>
+            </button>
+          </form>
+          <Scale
+            handleTempToggle={handleTempToggle}
+            toggleScale={toggleScale}
+          />
+        </div>
+
+        {Object.keys(data).length !== 0 ? (
+          <div className="body">
+            {/* Location header */}
+            <div className="body-item location">
+              <h2>{data.location.name}</h2>
+              <h3>
+                {data.location.region} {data.location.region ? ", " : " "}
+                {data.location.country}
+              </h3>
+            </div>
+
+            <div className="body-item current">
+              {/* Column 1 */}
+              <div className="overview">
+                <img
+                  src={data.current.condition.icon}
+                  className="main-icon"
+                  alt="weather-icon"
+                />
+                <div className="overview-text">
+                  {toggleScale.temperature ? (
+                    <h1>{data.current.temp_f}&#8457;</h1>
+                  ) : (
+                    <h1>{data.current.temp_c}&#8451;</h1>
+                  )}
+                  <h3>{data.current.condition.text}</h3>
+                </div>
+              </div>
+              <div className="detailed-view">
+                {parameters.map((parameter, i) => (
+                  <ParameterCard parameter={parameter} i={i} data={data} />
+                ))}
               </div>
             </div>
-            <div className="detailed-view">
-              {parameters.map((parameter, i) => (
-                <ParameterCard parameter={parameter} i={i} data={data} />
-              ))}
-            </div>
-          </div>
 
-          <div className="body-item forecast">
-            <h2>7 Day Forecast</h2>
-            <div className="forecast-details">
-              {data.forecast.forecastday.map((day, i) => {
-                return (
-                  <Forecast toggleScale={toggleScale} perDayData={day} i={i} />
-                );
-              })}
+            <div className="body-item forecast">
+              <h2>7 Day Forecast</h2>
+              <div className="forecast-details">
+                {data.forecast.forecastday.map((day, i) => {
+                  return (
+                    <Forecast
+                      toggleScale={toggleScale}
+                      perDayData={day}
+                      i={i}
+                    />
+                  );
+                })}
+              </div>
             </div>
           </div>
-        </div>
-      ) : null}
-    </div>
-  );
+        ) : null}
+      </div>
+    );
 };
 
 export default Home;
